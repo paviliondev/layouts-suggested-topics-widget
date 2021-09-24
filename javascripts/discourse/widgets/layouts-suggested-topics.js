1c0cf5ff33e92835a1ab9900fe7f6e71b7930d0b
Keegan George <kgeorge13@gmail.com>
2021-09-24 08:57:36 -0700

    DEV: Consolidate widget pieces into a separate methods

--- javascripts/discourse/widgets/layouts-suggested-topics.js
+++ javascripts/discourse/widgets/layouts-suggested-topics.js
@@ -21,7 +21,6 @@ export default layouts.createLayoutsWidget('suggested-topics-list', {
   html(attrs) {
     const { topic } = attrs;
     const suggestedTopics = topic.model.suggestedTopics;
-    console.log(suggestedTopics);
 
     if (!suggestedTopics) return '';
 
@@ -49,29 +48,36 @@ createWidget('layouts-suggested-topic', {
     const { topic } = attrs;
     const contents = [];
 
-    const topicTitle = htmlDecode(topic.fancyTitle);
-    const topicUrl = topic.url;
-    const categoryColor = topic.category.color;
-    const categoryName = topic.category.name;
-    const categoryUrl = topic.category.url;
-    const categoryStyle = topic.category.siteSettings.category_style;
     const replyCount = topic.replyCount;
     const viewCount = topic.views;
     const activity = topic.last_posted_at;
-    const tags = topic.tags;
-    const tagStyle = topic.siteSettings.tag_style;
 
-    contents.push(
-      h(
-        'a.title.raw-topic-link.raw-link',
-        {
-          attributes: {
-            href: topicUrl,
-          },
+    contents.push(this.getTopicTitle(topic));
+    contents.push(this.getCategories(topic.category));
+    contents.push(this.getTags(topic.tags, topic.siteSettings.tag_style));
+    return contents;
+  },
+
+  getTopicTitle(topic) {
+    const topicTitle = htmlDecode(topic.fancyTitle);
+    const topicUrl = topic.url;
+
+    return h(
+      'a.title.raw-topic-link.raw-link',
+      {
+        attributes: {
+          href: topicUrl,
         },
-        topicTitle
-      )
+      },
+      topicTitle
     );
+  },
+
+  getCategories(category) {
+    const categoryColor = category.color;
+    const categoryName = category.name;
+    const categoryUrl = category.url;
+    const categoryStyle = category.siteSettings.category_style;
 
     const categoryInfo = [
       h(
@@ -86,27 +92,21 @@ createWidget('layouts-suggested-topic', {
       h('span.badge-category', h('span.category-name', categoryName)),
     ];
 
-    contents.push(
-      h(
-        `a.badge-wrapper.${categoryStyle}`,
-        {
-          attributes: {
-            href: categoryUrl,
-          },
+    return h(
+      `a.badge-wrapper.${categoryStyle}`,
+      {
+        attributes: {
+          href: categoryUrl,
         },
-        categoryInfo
-      )
+      },
+      categoryInfo
     );
-
-    contents.push(this.getTags(tags, tagStyle));
-    return contents;
   },
 
   getTags(tags, tagStyle) {
     const allTags = [];
     if (tags) {
       tags.forEach((tag) => {
-        console.log(tag);
         allTags.push(
           h(
             `a.discourse-tag.${tagStyle}`,
